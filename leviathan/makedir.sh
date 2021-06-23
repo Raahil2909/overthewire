d203df39b5e966af5472793f74fa806be463b74e
Raahil2909 <raahilbadiani@gmail.com>
2021-06-23 14:47:38 +0530

    completed leviathan

---
+++ leviathan/makedir.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+for i in {0..7}
+do
+  if [ $i -lt 10 ]
+  then
+    mkdir "level0$i"
+    touch "level0$i/level0$i.txt"
+  else
+    mkdir "level$i"
+    touch "level$i/level$i.txt"
+  fi
+done
